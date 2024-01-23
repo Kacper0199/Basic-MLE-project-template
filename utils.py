5619fb132de344681b20ebe25c04bbeb79046609
Kacper Kozik <94921153+Kacper0199@users.noreply.github.com>
2024-01-23 15:57:11 +0100

    Init utils.py

---
+++ utils.py
@@ -0,0 +1,18 @@
+import os
+import logging
+
+
+def get_project_dir(sub_dir: str) -> str:
+    """Return path to a project subdirectory."""
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), sub_dir))
+
+
+def join_path(root_dir, target_dir):
+    """Return joined path. Works for Windows."""
+    return os.path.join(os.sep, root_dir + os.sep, target_dir)
+
+
+def configure_logging() -> None:
+    """Configures logging."""
+    logging.basicConfig(level=logging.INFO, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
